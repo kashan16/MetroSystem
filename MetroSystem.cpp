2f7c06d5578527bedd2bc7e43430b38ec8c9ee00
Mohd Kashan Yunus <106725612+kashan16@users.noreply.github.com>
2023-11-13 12:59:15 +0530

    Update MetroSystem.cpp

--- MetroSystem.cpp
+++ MetroSystem.cpp
@@ -1,192 +1,145 @@
-/* METRO MANAGEMENT
-A c++ program that uses different algorithms like Dijkstra(to print minimum distance)
-,breadth-first search, depth-first search, etc,the shortest path between the source station
-and the destination station is determined, and accordingly,the fare is being calculated on
-the basis of the total distance between the two stations.A graph is used to store the station(vertex) and distance(weight of edges)
-Finally, the metro route between the two stations and the total fare(minimum distance * fare{predetermined}) is displayed.*/
-
 #include <bits/stdc++.h>
-#include <math.h>
-#include <fstream>
-#include <string>
 #include <vector>
 #include <queue>
-#include <stack>
 #include <iostream>
+#include <map>
 using namespace std;
 
-class Dijkstra
-{
-    public:
-        vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
-        {
-            vector<int>dis(V,INT_MAX);
-            priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-            dis[S]=0;
-            pq.push({0,S});
-            while(!pq.empty())
-                {
-                    auto temp =pq.top();
-                    pq.pop();
-                    for(auto x : adj[temp.second])
-                        {
-                            int src=x[0];
-                            int val=x[1];
-                            if(dis[src]>temp.first+val)
-                                {
-                                    dis[src]=temp.first+val;
-                                    pq.push({dis[src],src});
-                                }
-                        }
-                    }
-            return dis;
+class Dijkstra {
+public:
+    vector<int> dijkstra(int V, vector<vector<pair<int, int>>> &adj, int S) {
+        vector<int> dis(V, INT_MAX);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        dis[S] = 0;
+        pq.push({0, S});
+        while (!pq.empty()) {
+            auto temp = pq.top();
+            pq.pop();
+            for (auto x : adj[temp.second]) {
+                int dest = x.first;
+                int val = x.second;
+                if (dis[dest] > temp.first + val) {
+                    dis[dest] = temp.first + val;
+                    pq.push({dis[dest], dest});
+                }
+            }
         }
+        return dis;
+    }
 };
-// To add an station and distance
-void addEdge(vector<vector<int>> adj[], int u, int v, int wt)
-{
-    vector<int> t1;
-    t1.push_back(v);
-    t1.push_back(wt);
-    adj[u].push_back(t1);
+
+void displayMetroMap() {
+    // Print a visual representation of the metro map
+    // You can use station names and connections to make it user-friendly
+    cout << "------ METRO MAP ------" << endl;
+    cout << "Station1 --- Station2 --- Station3 --- Station4" << endl;
+    cout << " |           |           |           |" << endl;
+    cout << "Station5 --- Station6 --- Station7 --- Station8" << endl;
+    cout << "             |           |" << endl;
+    cout << "          Station9" << endl;
+    cout << "-----------------------" << endl;
 }
 
-// Print adjacency list representation of graph
-void Prepare(vector<vector<int>> adj[])
-{
-    addEdge(adj,0,1,10);
-    addEdge(adj,1,0,10);
-    addEdge(adj,0,7,20);
-    addEdge(adj,7,0,20);
-    addEdge(adj,1,2,40);
-    addEdge(adj,2,1,40);
-    addEdge(adj,2,3,70);
-    addEdge(adj,3,2,70);
-    addEdge(adj,3,4,110);
-    addEdge(adj,4,3,110);
-    addEdge(adj,4,5,78);
-    addEdge(adj,5,4,78);
-    addEdge(adj,5,6,36);
-    addEdge(adj,6,5,36);
-    addEdge(adj,6,7,70);
-    addEdge(adj,7,6,70);
-    addEdge(adj,7,1,50);
-    addEdge(adj,1,7,50);
-    addEdge(adj,7,8,42);
-    addEdge(adj,8,7,42);
-    addEdge(adj,6,8,20);
-    addEdge(adj,8,6,20);
-    addEdge(adj,2,8,80);
-    addEdge(adj,8,2,80);
-    addEdge(adj,2,5,90);
-    addEdge(adj,5,2,90);
-    addEdge(adj,3,5,40);
-    addEdge(adj,5,3,40);
+void getTravelInformation(vector<vector<pair<int, int>>> &adj, map<string, int> &stationIndices) {
+    Dijkstra d; // Assuming you have a Dijkstra class or equivalent for pathfinding
+
+    // Ask the user for source and destination stations
+    string source, destination;
+    cout << "Enter your source station: ";
+    cin >> source;
+    cout << "Enter your destination station: ";
+    cin >> destination;
+    int fareRate = 12;
+    // Get numerical indices corresponding to station names
+    int srcIndex = stationIndices[source];
+    int destIndex = stationIndices[destination];
+
+    // Use Dijkstra's algorithm to get the shortest path and distance
+    vector<int> distances = d.dijkstra(9, adj, srcIndex);
+    int distance = distances[destIndex];
+
+    // Display travel information
+    cout << "Shortest distance between " << source << " and " << destination << " is: " << distance << " units" << endl;
+    cout << "Fare: " << distance * fareRate << " units" << endl;
 }
-// Driver code
-int menu()
-{
-    int choice1,choice;
-    Dijkstra d;
-    cout<<"------ WELCOME TO METRO MANAGEMENT ------"<<endl;
-    cout<<"For MAP Press 1"<<endl;
-    cout<<"For Travel INFO between 2 station Press 2"<<endl;
-    cin>>choice;
-    if(choice == 1)
-    {
-        system("cls");
-        for(int i = 0 ; i < 9 ; i++)
-            {
-                vector<int> res = d.dijkstra(9,adj,i);
-                for(int j = 0 ; j < 9 ; j++)
-                {
-                    if(i != j)
-                        {
-                            cout<<i<<" -> "<<j<<" Distance : "<<res[j]<<endl;
-                        }
-                }
-            }
-            cout<<"For main menu enter 1 : ";
-            cout<<"To exit press 2 :";
-            cin>>choice1;
-            if(choice1 == 1)
-            {
-                menu();
-            }
-            else return 0;
+
+
+// To add a station and distance
+void addEdge(vector<vector<pair<int, int>>> &adj, map<string, int> &stationIndices, const string &src, const string &dest, int wt) {
+    if (stationIndices.find(src) == stationIndices.end()) {
+        int newIndex = stationIndices.size();
+        stationIndices[src] = newIndex;
     }
-    if(choice == 2)
-    {
-        Dijkstra d;
-        system("cls");
-        int src,dest;
-        cout<<"Enter your station number : ";
-        cin>>src;
-        cout<<"Enter destination station number : ";
-        cin>>dest;
-        vector<int> res = d.dijkstra(9,adj,src);
-    	for(int i=0; i<9; i++)
-    	{
-    	    if(dest == i)
-            {
-                cout<<"Distance B/W "<<src<<" and "<<dest<<" is : "<<res[i];
-            }
-    	}
-    	cout<<endl;
-        cout<<"For main menu enter 1 : ";
-        cout<<"To exit press 2 :";
-        cin>>choice1;
-        if(choice1 == 1)
-        {
-            menu();
+    if (stationIndices.find(dest) == stationIndices.end()) {
+        int newIndex = stationIndices.size();
+        stationIndices[dest] = newIndex;
+    }
+
+    int u = stationIndices[src];
+    int v = stationIndices[dest];
+
+    adj[u].push_back({v, wt});
+    adj[v].push_back({u, wt}); // Assuming the graph is undirected
+}
+
+// Print adjacency list representation of the graph
+void Prepare(vector<vector<pair<int, int>>> &adj, map<string, int> &stationIndices) {
+    addEdge(adj, stationIndices, "Station1", "Station2", 10);
+    addEdge(adj, stationIndices, "Station2", "Station3", 20);
+    addEdge(adj, stationIndices, "Station3", "Station4", 15);
+    addEdge(adj, stationIndices, "Station4", "Station5", 25);
+    addEdge(adj, stationIndices, "Station5", "Station6", 18);
+    addEdge(adj, stationIndices, "Station6", "Station7", 12);
+    addEdge(adj, stationIndices, "Station7", "Station8", 30);
+    addEdge(adj, stationIndices, "Station8", "Station9", 22);
+    // Add more stations and connections as needed
+}
+
+int menu(vector<vector<pair<int, int>>> &adj, map<string, int> &stationIndices) {
+    int choice;
+    do {
+        cout << "------ WELCOME TO METRO MANAGEMENT ------" << endl;
+        cout << "1. View Metro Map" << endl;
+        cout << "2. Get Travel Information between Stations" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                // Call a function to display the metro map
+                displayMetroMap();
+                break;
+            case 2:
+                // Call a function to get travel information
+                getTravelInformation(adj,stationIndices);
+                break;
+            case 3:
+                cout << "Exiting the program. Goodbye!" << endl;
+                return 0; // or exit(0);
+            default:
+                cout << "Invalid choice. Please enter a valid option." << endl;
         }
-        else return 0;
-    }
+
+        cout << "Do you want to return to the main menu? (1 for Yes, 0 for No): ";
+        cin >> choice;
+
+    } while (choice == 1);
+
+    return 0;
 }
-int main()
-{
-    int V = 9;
-    vector<vector<int>> adj[V];
-    vector<bool> visited(V,false);
-    Prepare(adj);
-    /*cout<<"------ WELCOME TO METRO MANAGEMENT ------"<<endl;
-    cout<<"For MAP Press 1"<<endl;
-    cout<<"For Travel INFO between 2 station Press 2"<<endl;*/ 
-    menu();
-    /*Dijkstra d;
-    if(choice == 1)
-    {
-        system("cls");
-        for(int i = 0 ; i < 9 ; i++)
-            {
-                vector<int> res = d.dijkstra(9,adj,i);
-                for(int j = 0 ; j < 9 ; j++)
-                {
-                    if(i != j)
-                        {
-                            cout<<i<<" -> "<<j<<" Distance : "<<res[j]<<endl;
-                        }
-                }
-            }
-    }
-    if(choice == 2)
-    {
-        Dijkstra d;
-        system("cls");
-        int src,dest;
-        cout<<"Enter your station number : ";
-        cin>>src;
-        cout<<"Enter destination station number : ";
-        cin>>dest;
-        vector<int> res = d.dijkstra(9,adj,src);
-    	for(int i=0; i<9; i++)
-    	{
-    	    if(dest == i)
-            {
-                cout<<"Distance B/W "<<src<<" and "<<dest<<" is : "<<res[i];
-            }
-    	}
-    	cout<<endl;
-    }*/
+
+
+int main() {
+
+    vector<vector<pair<int, int>>> adj(9);
+    map<string, int> stationIndices;
+
+    Prepare(adj, stationIndices);
+
+    Dijkstra d;
+
+    menu(adj, stationIndices);
+
     return 0;
 }
